6b779c8ecef945edefc1d9af8bac820ac9d409a6
bestsabplayerox <codynutkisslover@gmail.com>
2026-03-25 11:43:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,7 @@ var APPS = {
     'term': {title: 'Spotify', path: 'script/Apps/Spotify/index.html', icon: 'https://cdn.pixabay.com/photo/2016/10/22/00/15/spotify-1759471_1280.jpg', pinned: true},
     'files': {title: 'PS5 Emu', path: 'script/Apps/Ps5/index.html', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-OeL_be7RFaoHi3PswkuAR5XcMgBNRDynsg&s', pinned: true},
     'web': {title: 'Cine-Web', path: 'script/Apps/Web/index.html', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeD89ZcX5W1FBtal7RerasT27q-OmZqnBixQ&s', pinned: true},
-    'roblox': {
-    title: 'Roblox', 
-    path: 'script/Apps/Roblox/index.html', 
-    icon: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Roblox_player_icon_2022.svg', 
-    pinned: false
-},
+    'roblox': {title: 'Roblox', path: 'script/Apps/Roblox/index.html', icon: 'https://cdn.icon-icons.com/icons2/3915/PNG/512/roblox_logo_icon_249221.png', pinned: false},
     'settings': {title: 'CONFIG', internal: true, icon: 'https://cdn.iconscout.com/icon/free/png-256/free-apple-settings-icon-svg-download-png-493162.png', pinned: true},
     'discord': {title: 'Discord', path: 'https://discord.com/app', icon: 'https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png', pinned: false}
 };
